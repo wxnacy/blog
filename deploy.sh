7bba1d245eff42ac46770a4dc4a6ce825de08d7c
wxnacy <wxnacy@gmail.com>
2020-09-27 00:39:08 +0800

    Add: 增加 发布页面脚本

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,37 @@
-#!/bin/bash
-set -xe
+#!/usr/bin/env bash
+# Author: wxnacy(wxnacy@gmail.com)
+# Description: 发布到 oss
+# ossutil 文档: https://help.aliyun.com/document_detail/120075.html?spm=a2c4g.11186623.6.732.36381c7aWzhGGo
+# 安装
+# curl -o /usr/local/bin/ossutil http://gosspublic.alicdn.com/ossutil/1.6.18/ossutilmac64 && chmod 755 /usr/local/bin/ossutil
 
-POSTS_PATH='/opt/hexo/source/_posts'
+env=$1
+bucket=blog-test-cn
+if [[ $env == 'prod' ]]
+then
+  bucket=blog-cn
+fi
+echo "部署 ${bucket}"
 
-echo "Start generation and deployment"
-cd $POSTS_PATH
-echo "pulling source code..."
-git pull origin master
-echo "generate and deploy..."
-cd /opt/hexo
-hexo clean
-hexo g
-echo "Finished."
+ossutil -i ${OSS_ACCESS_KEY_ID} -k ${OSS_ACCESS_KEY_SECRET} -e ${OSS_ENDPOINT} \
+  cp public oss://${bucket}/ -rfu --jobs 100
+
+# pytext="import os;names = os.listdir('public'); print(names)"
+
+# files=`python -c "import os;names = os.listdir('public'); names =['public/' + o + '/' if os.path.isdir('public/' + o) else 'public/' +o for o in names]; print(str(names))"`
+# files=`python -c ${pytext}`
+# echo $files
+
+
+# for f in  $files
+# do
+  # echo $f
+  # if [[ $f == */ ]]
+  # then
+    # ossutilmac64 cp $f oss://${bucket}/$f -rfu
+  # else
+    # ossutilmac64 cp $f oss://${bucket}/$f -fu
+  # fi
+
+# done
+
