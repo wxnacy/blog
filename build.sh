3f027bbf1b540f110846165a46cf6c69e0aa5882
wxnacy <wxnacy@gmail.com>
2021-04-17 20:49:07 +0800

    U: 迁移脚本

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Author: wxnacy(wxnacy@gmail.com)
-# Description: 
-
-docker build -t wxnacy/hexo:1.0.0 .
-docker build -t wxnacy/hexo:latest .
-docker-compose up --force-recreate -d blog
